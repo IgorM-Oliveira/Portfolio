ddcd1f301a34ef1c4926e6279e5fa9fca7a0dcc7
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-03-25 00:11:30 -0400

    initial implement card projects

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Typography, Row, Col} from "antd";
+import {Typography, Row, Col, Card} from "antd";
 import {useSelector} from "react-redux";
 import {selectTheme} from "../store/themeReducer";
 
@@ -14,6 +14,15 @@ export default function Home() {
         <div className={'content'}>
           <Title className={`gradient ${mode}`}>Projetos</Title>
           <Paragraph className={`${mode}`}>Front-end Developer</Paragraph>
+
+          <Row gutter={[16, 16]}>
+            <Col lg={{span: 6}} sm={{span: 12}} >
+              <div onClick={() => console.log('teste')} style={{cursor: 'pointer', backgroundColor: 'rgba(52, 52, 52, 0.3)', padding: '20px', borderRadius: '20px'}}>
+                <Title level={2}>Teste</Title>
+                <Paragraph>Teste do teste que foi construido para o teste</Paragraph>
+              </div>
+            </Col>
+          </Row>
         </div>
       </Col>
     </Row>
